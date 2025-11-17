7bb721e7bc4b5bebd0762b81d2a7ac85888ec82c
1111x <russellsaintcyr@gmail.com>
2025-11-17 16:44:52 -0500

    Added gh-pages support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/brianstcyr-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/brianstcyr-portfolio/' : '',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
