39bbf5f250de714e806737e8ed1604d27e4a7d8f
1111x <russellsaintcyr@gmail.com>
2025-11-17 16:41:28 -0500

    Layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         </div>
 
         {/* Portfolio Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-1">
           {portfolioData.items.map((item) => (
             <Link 
               key={item.id} 
