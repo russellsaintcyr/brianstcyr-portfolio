baa9940ffd775a40f301d876fb2a6a6461e87b17
1111x <russellsaintcyr@gmail.com>
2025-12-10 21:11:41 -0500

    Removed GH pages

--- next.config.ts
+++ next.config.ts
@@ -1,56 +1,26 @@
 
 import type { NextConfig } from "next";
 
-const isGitHubPages = process.env.GITHUB_ACTIONS || process.env.EXPORT_MODE;
-
 const nextConfig: NextConfig = {
-  ...(isGitHubPages && {
-    output: 'export',
-    trailingSlash: true,
-    basePath: '/brianstcyr-portfolio',
-    assetPrefix: '/brianstcyr-portfolio/',
-    images: {
-      unoptimized: true,
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.ctfassets.net',
-          port: '',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'cdn.myportfolio.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-    env: {
-      NEXT_PUBLIC_BASE_PATH: '/brianstcyr-portfolio',
-    },
-  }),
-  ...(!isGitHubPages && {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.ctfassets.net',
-          port: '',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'cdn.myportfolio.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-    env: {
-      NEXT_PUBLIC_BASE_PATH: '',
-    },
-  })
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.myportfolio.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: '',
+  },
 };
 
 export default nextConfig;
